731030d36b5780f47bd728ca40a39ae304beff5a
Juan Pablo <jp.lopez@stuart.com>
2024-09-10 08:19:55 +0200

    Fix Bug Max Value

--- core/src/main/java/com/graphhopper/routing/weighting/custom/CustomModelParser.java
+++ core/src/main/java/com/graphhopper/routing/weighting/custom/CustomModelParser.java
@@ -71,7 +71,7 @@ public class CustomModelParser {
                                                   EncodedValueLookup lookup, TurnCostProvider turnCostProvider, CustomModel customModel) {
         if (customModel == null)
             throw new IllegalStateException("CustomModel cannot be null");
-        double maxSpeed = speedEnc.getMaxOrMaxStorableDecimal();
+        double maxSpeed = speedEnc.getMaxStorableDecimal();
         CustomWeighting.Parameters parameters = createWeightingParameters(customModel, lookup, speedEnc, maxSpeed, priorityEnc);
         return new CustomWeighting(accessEnc, speedEnc, turnCostProvider, parameters);
     }
